503bd2ef428eb7e30e9e2e7919b1cae6d0029bfb
p-febis <preston.febis@yahoo.com>
2025-07-11 16:19:58 -0300

    Added an Experience section

--- src/features/AboutPage/components/AboutPage.tsx
+++ src/features/AboutPage/components/AboutPage.tsx
@@ -1,6 +1,7 @@
 import { LinedText } from "@/components/LinedText";
 import { HeroSection } from "@/features/AboutPage/components/HeroSection";
 import { AboutMeCards } from "@/features/AboutPage/components/AboutMeCards";
+import { ExperienceTimeline } from "./ExperienceCards";
 
 const AboutPage = () => {
   return (
@@ -8,6 +9,8 @@ const AboutPage = () => {
       <HeroSection />
       <LinedText title="About Me" />
       <AboutMeCards />
+      <LinedText title="Experience" />
+      <ExperienceTimeline />
     </>
   );
 };
